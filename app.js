b787510bc210c64275b11f3cd9ca5a11d53065ac
Artyom <artyom-first@yandex.ru>
2022-11-05 12:02:31 +0500

    refactor: создать централизованный роут

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 // основной файл сервера
 require('dotenv').config();
 const express = require('express'); // фреймворк express для NodeJS
-const bodyParser = require('body-parser'); // анализирует тела входящих запросов в промежуточном программном обеспечении
 const mongoose = require('mongoose'); // база данных NongoDB
-const { errors } = require('celebrate');
-const usersRouter = require('./routes/usersRouter');
-const moviesRouter = require('./routes/moviesRouter');
-const signInRouter = require('./routes/signInRouter');
-const signUpRouter = require('./routes/signUpRouter');
-const auth = require('./middlewares/auth');
-const errorHandler = require('./middlewares/errorHandler');
-const NotFoundError = require('./errors/NotFoundError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const mainRouter = require('./routes/index'); // файл со всеми роутами
 const mongoAddress = require('./utils/constants');
 
 const { NODE_ENV, DB_ADDRESS } = process.env;
@@ -19,7 +10,7 @@ const { NODE_ENV, DB_ADDRESS } = process.env;
 const { PORT = 3050 } = process.env; // файл .env хранится на сервере
 const app = express(); // app работает через фреймворк Express
 
-const allowedCors = [
+const allowedCors = [ // разрешённые домены для запросов на этот сервер
   'http://localhost:3000',
 ];
 
@@ -45,21 +36,7 @@ app.use((req, res, next) => {
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : mongoAddress, { // подключение к базе MongooseDB
   useNewUrlParser: true,
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(requestLogger);
-
-app.use('/signin', signInRouter); // авторизация пользователя
-app.use('/signup', signUpRouter); // регистрация пользователя
-app.use(auth); // проверка токена
-app.use('/users', usersRouter); // пути для работы с карточками
-app.use('/movies', moviesRouter); // пути для работы с пользователем
-app.use('/', (req, res, next) => { next(new NotFoundError('страница не найдена')); }); // введён неизвестный путь
-app.use(errors()); // обработка ошибок библиотеки celebrate
-app.use(errorLogger); // логи
-app.use(errorHandler); // обработка ошибок сервера
-
+app.use(mainRouter);
 app.listen(PORT, () => { // при запуске сервера выводит его порт
   console.log(`Порт сервера: ${PORT}`);
 });
